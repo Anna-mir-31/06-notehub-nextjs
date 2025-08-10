0ae50e49e8993de349d6af60729b1ba12eb6e9ba
Anna Chernobrovenko <annanyuska2404@gmail.com>
2025-08-10 19:22:48 +0200

    Update page.tsx

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,28 +1,14 @@
-import { fetchNoteById } from "../../../lib/api";
-import {
-  QueryClient,
-  HydrationBoundary,
-  dehydrate,
-} from "@tanstack/react-query";
-import NoteDetailsClient from "./NoteDetails.client";
+import { fetchNoteById } from "@/lib/api";
+import NoteDetailsClient from "./NoteDetails.client"; // без alias і дужок у шляху безпечніше для TS
+import { Note } from "@/types/note";
 
-type Props = {
-  params: { id: string }; // було Promise<{ id: string }>
-};
+type Params = { id: string };
 
-const NoteDetails = async ({ params: { id } }: Props) => {
-  const queryClient = new QueryClient();
-
-  await queryClient.prefetchQuery({
-    queryKey: ["note", id],
-    queryFn: () => fetchNoteById(id),
-  });
-
-  return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <NoteDetailsClient />
-    </HydrationBoundary>
-  );
-};
-
-export default NoteDetails;
+export default async function NoteDetailsPage({
+  params,
+}: {
+  params: Params;
+}) {
+  const note: Note = await fetchNoteById(params.id);
+  return <NoteDetailsClient note={note} />;
+}
