3a600852367ae67eb039a99f54c2979ac08e6b94
Anna_Chernobrovenko <annanyuska2404@gmail.com>
2025-08-13 22:33:45 +0200

    fixpage

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -5,6 +5,7 @@ interface NoteDetailsProps {
 export default async function NoteDetails({ params }: NoteDetailsProps) {
   const { id } = await params;
 
-  // тут твоя логіка
-  return <div>Note ID: {id}</div>;
+  const note: Note = await fetchNoteById(id);
+
+  return <NoteDetailsClient note={note} />;
 }
