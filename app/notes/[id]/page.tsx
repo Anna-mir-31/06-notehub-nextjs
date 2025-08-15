59ea922dd3bd2a3b7e9ac8f9a164385a7a6e1482
Anna_Chernobrovenko <annanyuska2404@gmail.com>
2025-08-15 15:08:10 +0200

    index

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,22 +1,10 @@
-import { notFound } from 'next/navigation';
-import { fetchNoteById } from '@/lib/api';
-import type { Note } from '@/types/note';
-
-type Props = {
-  params: Promise<{ id: string }>;
-};
-
-export default async function NoteDetailsPage({ params }: Props) {
-  const { id } = await params;
+export default function NoteDetailsPage() {
 
  
-    const note: Note = await fetchNoteById(id);
-    return (
-      <div style={{ padding: 24 }}>
-        <h1>{note.title}</h1>
-        <p style={{ whiteSpace: 'pre-wrap' }}>{note.content}</p>
-        <p><strong>Tag:</strong> {note.tag}</p>
-      </div>
-    );
-  
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>details</h1>
+     </div>
+  );
+
 }
